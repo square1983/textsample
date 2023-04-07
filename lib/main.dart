2adb35d22baff460483e88701716c29382250421
suya <suya@192.168.0.103>
2023-04-07 11:36:31 +0800

    没有问题啊

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:image_issue/thumb_item.dart';
+
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 void main() {
@@ -25,6 +27,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// 这个例子没有问题。。
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -39,6 +42,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<AssetEntity> _assets = [];
   List<File> _images = [];
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
 
   @override
@@ -46,25 +51,103 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
 
   }
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-
+    print('build time ${DateTime.now()}}');
     return Scaffold(
       appBar: AppBar(
 
         title: Text(widget.title),
       ),
-      body: ListView.separated(
-          padding: const EdgeInsets.all(16),
-          itemBuilder: (BuildContext context,int index){
-        return ThumbItem(
-          width: MediaQuery.of(context).size.width-32,
-          height: 300, file: _images[index],
-        );
-      }, separatorBuilder: (BuildContext context,int index){
-        return const SizedBox(height: 16,);
-      }, itemCount: _images.length),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(' ${_images.length} images \n email: ${_emailController.text} \n password: ${_passwordController.text}'),
+            Container(
+              height: 300,
+              child: Swiper(
+                  itemCount: _images.length,
+                  itemBuilder: (BuildContext context,int index){
+                return Container(
+                  margin: const EdgeInsets.all(16),
+                  width: MediaQuery.of(context).size.width,
+                  height: 300,
+                  child: Stack(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width-32,
+                        height: 300,
+                        child: ThumbItem(
+                          width: MediaQuery.of(context).size.width-32,
+                          height: 300, file: _images[index],fit: BoxFit.cover,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                          icon: const Icon(Icons.close),
+                          onPressed: (){
+                            setState(() {
+                              _assets.removeAt(index);
+                              _images.removeAt(index);
+                            });
+                          },
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                          decoration:  BoxDecoration(
+                            color: Colors.black.withOpacity(0.5),
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(8),
+                            ),
+                          ),
+
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(index.toString(),style: TextStyle(color: Colors.white,fontSize: 32),),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your email',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your password',
+
+                ),
+                obscureText: true,
+              ),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _pickImages,
         tooltip: 'Increment',
@@ -85,4 +168,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
     setState(() {});
   }
+
+
 }
